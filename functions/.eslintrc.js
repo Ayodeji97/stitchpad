b8a14b42a6f0a97bc6285241e0c09d89e8168a78
Daniel Ogunleye <danielayodeji97@gmail.com>
2026-05-12 13:01:03 +0100

    chore(functions): scaffold Cloud Functions TypeScript project

---
+++ functions/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: { node: true, es2022: true, jest: true },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+    project: ['./tsconfig.dev.json'],
+  },
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  ignorePatterns: ['/lib/**/*', '/node_modules/**/*'],
+  rules: {
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    '@typescript-eslint/no-explicit-any': 'warn',
+  },
+};
